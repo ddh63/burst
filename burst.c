416dcc8830bd81a9214eff9718ecb5d8f95206fc
ddh63 <ddh63@zips.uakron.edu>
2017-04-17 23:49:04 +0000

    file is read and blocks are sent to stdout

--- burst.c
+++ burst.c
@@ -8,6 +8,10 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+#define BLOCK 500
 
 int main(int argc, char* argv[]) {  
 
@@ -17,13 +21,24 @@ int main(int argc, char* argv[]) {
 	}
 
 	int infd = open(argv[1], O_RDONLY);
-	if (infd < 0) {
-		perror("Input open error");
-		return 1;
+  if (infd < 0) {
+    perror("Input open error");
+    return 1;
+  }
+	
+	char buf[BLOCK];
+	while (1) {
+		int bytesread = read(infd, buf, BLOCK);
+		fprintf(stderr, "\nBytes: %d\n", bytesread);
+		if ((bytesread == -1) && (errno == EINTR))
+			continue;
+
+		if (bytesread == 0)
+			break;
+
+		write(STDOUT_FILENO, buf, bytesread);
 	}
 
-	fprintf(stderr, "filedes: %d\n", infd);
-
 	close(infd);
-  return 0;
+	return 0;
 }
