226e03f78c73fed3d58e01682ba12d01c8443f31
ddh63 <ddh63@zips.uakron.edu>
2017-04-17 15:54:26 +0000

    file input added

--- burst.c
+++ burst.c
@@ -4,10 +4,26 @@
   reads in txt file and separates them into 500 line segments
 */
 
+#include <unistd.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int main() {
-  
-  fprintf(stderr, "Test\n");
+int main(int argc, char* argv[]) {  
+
+	if (argc < 2) {
+		fprintf(stderr, "usage: burst <input file>\n");
+		return 1;
+	}
+
+	int infd = open(argv[1], O_RDONLY);
+	if (infd < 0) {
+		perror("Input open error");
+		return 1;
+	}
+
+	fprintf(stderr, "filedes: %d\n", infd);
+
+	close(infd);
   return 0;
 }
