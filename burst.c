8e529a7054d58fa49165e650342c642c4bdc99c5
ddh63 <ddh63@zips.uakron.edu>
2017-04-18 00:37:41 +0000

    creates files based on original filename and writes to them

--- burst.c
+++ burst.c
@@ -27,6 +27,9 @@ int main(int argc, char* argv[]) {
   }
 	
 	char buf[BLOCK];
+	// used to get output filename
+	int count = 1;
+
 	while (1) {
 		int bytesread = read(infd, buf, BLOCK);
 		fprintf(stderr, "\nBytes: %d\n", bytesread);
@@ -35,10 +38,38 @@ int main(int argc, char* argv[]) {
 
 		if (bytesread == 0)
 			break;
+		
+		char* point;
+		char file[100];
+		
+		/* gets point where . is to get filename and  full file extension */
+		point = strchr(argv[1], '.');
 
-		write(STDOUT_FILENO, buf, bytesread);
+		// adds filename to file
+		strncpy(file, argv[1], point-argv[1]);
+		file[point-argv[1]] = '\0';
+
+		// puts file extension into a variable
+		char ext[20];
+		strcpy(ext, &argv[1][(int)(point-argv[1])]);
+
+		// puts filename all together and increments count for next file
+		sprintf(file, "%s-%d%s", file, count, ext);
+		printf("%s\n", file);
+		count++;
+
+		int outfd = open(file, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+		if (outfd < 0) {
+			perror("Output open error");
+			return 1;
+		}
+
+		write(outfd, buf, bytesread);
+
+		close(outfd);
 	}
 
 	close(infd);
+
 	return 0;
 }
